4728ddbdffbce26f95893afc75c4948eb29ace36
Stefan Schlesinger <sts@ono.at>
2012-04-23 21:16:53 +0200

    Show and tell

--- lib/smailr/alias.rb
+++ lib/smailr/alias.rb
@@ -5,22 +5,12 @@ module Smailr
             alias_localpart, alias_fqdn = options.alias.split('@')
 
             # Check if alias_fqdn is a local domain
-            alias_domain = Model::Domain[:fqdn => alias_fqdn]
-            if not alias_domain.is_a(Model::Domain)
-                error 'You are trying to add an alias for a non-local domain.'
+            if Model::Domain.where(:fqdn => alias_fqdn).empty?
+                say_error "You are trying to add an alias for a non-local domain: #{alias_fqdn}"
             end
-        
-            #domain = Model::Domain[:fqdn => fqdn]
-            #mbox   = Model::Mailbox.create(:localpart => localpart, :password => password)
-            #domain.add_mailbox(mbox)
-        end
-        
-        def self.rm(address, options)
-            localpart, fqdn = address.split('@')
-        
-            mbox   = Model::Mailbox[:localpart => localpart]
-            domain = Model::Domain[:fqdn => fqdn]
-            domain.remove_mailbox(mbox)
+
+            mbox = Model::Mailbox[:localpart => mbox_localpart, :domain => mbox_fqdn]
+            mbox.add_alias(:address => options.alias)
         end
     end
 end
